f1a8e07e2c0b7662457db22acbbd5bf7f9771a69
Erwan Goudard <erwan.goudard@qonto.com>
2023-04-12 10:28:27 +0200

    Update eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "plugin:vue/vue3-essential",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 2015,
+  },
+  plugins: ["vue"],
+  rules: {},
+};
