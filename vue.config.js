f43278d07e8534e05ec6f69aa74007b519c01207
Erwan Goudard <erwan.goudard@qonto.com>
2023-04-12 14:38:09 +0200

    Move build folder to docs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/frontendtest/'
 
+  publicPath: process.env.NODE_ENV === "production" ? "/frontendtest/" : "/",
 });
